14ed83d87a76b575351007f4defb608f8a3da436
DarinVeytsman <72801650+DarinVeytsman@users.noreply.github.com>
2020-10-24 16:15:08 +0500

    123

---
+++ jshome/script.js
@@ -0,0 +1,18 @@
+
+// let factr = 3;
+// let res = 1;
+
+// for (let i = 1; i < factr; i++) {
+//     res = res * i;
+//     console.log(res); 
+
+// }
+// alert(res);
+
+var a = prompt('Введитет число');
+var b = 1;
+
+for ( i = 0; i < a; i++) {
+    b = b * ( a -  i );
+}
+alert(a + ' ! = ' + b);
